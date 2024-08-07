ef37110086bbc6ac7d89254cff1b2cdd572c4b46
gosh-eih6EG <wiktor.koziak@bbh.com>
2024-08-07 20:29:30 +0200

    Available books selector

--- src/app/book-list/books.selectors.ts
+++ src/app/book-list/books.selectors.ts
@@ -1,7 +1,11 @@
-import { createFeatureSelector } from '@ngrx/store';
+import { createFeatureSelector, createSelector } from '@ngrx/store';
 import { Book } from './books.model';
 import { booksFeatureKey } from './books.reducer';
+import { selectCollection } from '../book-collection/collections.selectors';
 
 export const selectBooks = createFeatureSelector<ReadonlyArray<Book>>(booksFeatureKey);
 
 // TODO: write selectAvailableBooks selector - selects only books that are NOT part of collection
+export const selectAvailableBooks = createSelector(selectBooks, selectCollection, (books, collection) => {
+  return books.filter(({ id }) => !collection.map((book) => book.id).includes(id));
+});
